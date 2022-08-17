c13cc3bdbeaed1379bf78f298df3ab18e37e2ff4
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2022-08-17 15:11:23 +0300

    Parsing logic using Decodable

---
+++ iMusic/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  iMusic
+//
+//  Created by Danya Kukhar on 17.08.2022.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+
+struct Track: Decodable {
+    var trackName: String
+    var collectionName: String?
+    var artistName: String
+    var artworkUrl100: String?
+}
